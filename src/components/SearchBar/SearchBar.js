db830e1db0949aa915bcbfc7b1b0e099ec05d8d3
Isaac Kwon <isaac-kwon@hotmail.com>
2021-03-07 10:57:03 -0600

    add custom styles for search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import SearchRounded from '@material-ui/icons/SearchRounded';
 import CloseOutlined from '@material-ui/icons/CloseOutlined';
@@ -7,10 +8,25 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import { searchLocation, clearSearch } from '../../actions';
 import styles from './SearchBar.module.css';
 
+// custom style for text field
+const useStyles = makeStyles(() =>
+  createStyles({
+    textField: {
+      '& label.Mui-focused': {
+        color: '#000000',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottom: '0.2rem solid #000000',
+      },
+    },
+  })
+);
+
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const search = useSelector(state => state.search);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   const handleSearch = e => setSearchTerm(e.target.value);
   const handleClear = () => {
@@ -26,7 +42,7 @@ const SearchBar = () => {
     <div>
       <TextField
         multiline
-        className={styles.searchbar}
+        className={`${styles.searchbar} ${classes.textField}`}
         value={searchTerm}
         label='Where are you headed?'
         placeholder='Airport or City'
